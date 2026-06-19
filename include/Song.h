03da279f1530b055ddd23bcb86b0b1d1e360401f
FabriceFF <fabrice.otchouang-njike@stud.hshl.ed>
2026-06-20 00:43:39 +0200

    First Push

--- include/Song.h
+++ include/Song.h
@@ -1,6 +1,7 @@
 //
 // Created by fabri on 19.06.2026.
 //
+include/Song.h
 
 #ifndef PLAYLISTMANAGEMENTSYSTEM_SONG_H
 #define PLAYLISTMANAGEMENTSYSTEM_SONG_H
--- src/Song.cpp
+++ src/Song.cpp
@@ -1,5 +1,28 @@
 //
 // Created by fabri on 19.06.2026.
 //
+#include <iostream>
+#ifndef SONG_H
+#define SONG_H
+#include <string>
 
-#include "Song.h"
+using namespace std;
+
+class Song {
+    private:
+    int duration;
+    string artist;
+    string title;
+
+    public: //constructor
+    Song(const int duration, const string& artist, const string& title);
+
+    // Read the privates
+    string get_artist() const;
+    string get_title() const;
+    int get_duration() const;
+
+    //Convert song to readable text
+    string to_string() const;
+};
+#endif
